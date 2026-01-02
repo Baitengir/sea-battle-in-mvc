e2ec5736391f3de8b4296cabac9965474b29e4f3
Baitenir <bakumake77@gmail.com>
2026-01-02 23:01:26 +0600

    first commit

---
+++ src/main/java/seabattleinmvc/SeaBattleInMvcApplication.java
@@ -0,0 +1,13 @@
+package seabattleinmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleInMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaBattleInMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seabattleinmvc/SeaBattleInMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package seabattleinmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleInMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
