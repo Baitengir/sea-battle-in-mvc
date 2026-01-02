e7dd64103f5a4d94cf2ed582b663f8f8fbf8aed1
Baitenir <bakumake77@gmail.com>
2026-01-02 23:22:15 +0600

    model user is readey

---
+++ src/main/java/seabattleinmvc/controller/UserController.java
@@ -0,0 +1,4 @@
+package seabattleinmvc.controller;
+
+public class UserController {
+}
--- src/main/java/seabattleinmvc/models/User.java
+++ src/main/java/seabattleinmvc/models/User.java
@@ -1,4 +1,19 @@
 package seabattleinmvc.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+
 }
